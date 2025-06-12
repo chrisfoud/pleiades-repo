501c880f66b6c0ebcfd98a83baad6d3c968f16b2
chrisfoud <christos.foudoulis@cloudnomads.com.cy>
2025-06-12 15:16:37 +0300

    names modified

--- compute_infra/config.py
+++ compute_infra/config.py
@@ -2,7 +2,7 @@
 from dataclasses import dataclass, field
 import uuid
 from typing import List
-
+import common_config
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_elasticloadbalancingv2 as elbv2,
@@ -14,8 +14,8 @@ unique_suffix = str(uuid.uuid4())[:8].lower()
 @dataclass
 class VpcConfig:
     """Configuration for the VPC."""
-    VPC_CFN_ID: str = f"VPC-{unique_suffix}"
-    VPC_NAME: str = f"my-application-vpc-{unique_suffix}"
+    VPC_CFN_ID: str = f"vpc-{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}"
+    VPC_NAME: str = f"{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}-vpc"
     MAX_AZS: int = 2 # Number of Availability Zones to use
 
 @dataclass
@@ -30,8 +30,8 @@ class Ec2InstanceConfig:
 @dataclass
 class AlbConfig:
     """Configuration for the Application Load Balancer."""
-    ALB_CFN_ID: str = f"ALB-{unique_suffix}"
-    ALB_NAME: str = f"my-app-alb-{unique_suffix}"
+    ALB_CFN_ID: str = f"ALB-{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}"
+    ALB_NAME: str = f"{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}-alb"
     LISTENER_PORT: int = 80 # Port for the ALB listener (e.g., HTTP)
 
 @dataclass
@@ -61,17 +61,17 @@ AppInfrastructureConfig = InfrastructureConfig(
     ),
     EC2_INSTANCES=[
         Ec2InstanceConfig(
-            INSTANCE_CFN_ID=f"EC2Instance1-{unique_suffix}",
-            INSTANCE_NAME=f"WebAppInstance1-{unique_suffix}",
+            INSTANCE_CFN_ID=f"EC2-{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}-1",
+            INSTANCE_NAME=f"EC2-{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}-1",
             INSTANCE_TYPE=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            MACHINE_IMAGE=ec2.MachineImage.latest_amazon_linux(), # Use Amazon Linux 2
+            MACHINE_IMAGE=ec2.MachineImage.generic_windows({'us-east-1': 'ami-05e885aafb1fdb4dd'}),
             USER_DATA_SCRIPT=nginx_user_data
         ),
         Ec2InstanceConfig(
-            INSTANCE_CFN_ID=f"EC2Instance2-{unique_suffix}",
-            INSTANCE_NAME=f"WebAppInstance2-{unique_suffix}",
+            INSTANCE_CFN_ID=f"EC2-{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}-2",
+            INSTANCE_NAME=f"EC2-{common_config.ENV}-{common_config.COMMON_NAME}-{common_config.APP_NAME}-2",
             INSTANCE_TYPE=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            MACHINE_IMAGE=ec2.MachineImage.latest_amazon_linux(), # Use Amazon Linux 2
+            MACHINE_IMAGE=ec2.MachineImage.generic_windows({'us-east-1': 'ami-05e885aafb1fdb4dd'}),
             USER_DATA_SCRIPT=nginx_user_data
         ),
         # Add more Ec2InstanceConfig objects here to create more instances
